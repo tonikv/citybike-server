d217bf317ec4e32787f8c6ebd2b65b606a19acf1
Toni Virtanen <tonikoodaa@gmail.com>
2023-01-24 13:19:06 +0200

    Added required properties to models schema

--- models.js
+++ models.js
@@ -1,36 +1,38 @@
 const mongoose = require('mongoose');
-  
+
 // Journey Modal Schema
 const journeysSchema = new mongoose.Schema({
-    Departure: Date,
-    Return: Date,
-    Departure_station_id: Number,
-    Departure_station_name: String,
-    Return_station_id: Number,
-    Return_station_name: String,
-    Covered_distance: Number,
-    Duration: Number
+    Departure: { type:Date, required: true },
+    Return: { type:Date, required: true },
+    Departure_station_id: { type: Number, required: true },
+    Departure_station_name: { type: String, required: true },
+    Return_station_id: { type: Number, required: true },
+    Return_station_name: { type: String, required: true },
+    Covered_distance: { type: Number, required: true },
+    Duration: { type: Number, required: true },
 });
-  
+
 // Stations Modal Schema
 const stationsSchema = new mongoose.Schema({
-    FID: Number,
-    ID: Number,
-    Nimi: String,
-    Osoite: String,
-    Kaupunki: String,
-    Operaattori: String,
-    Kapasiteetti: Number,
-    x: Number,
-    y: Number,
+    FID: { type: Number, required: true, unique: true},
+    ID: { type: Number, required: true, unique: true},
+    Nimi: { type: String, required: true},
+    Osoite: { type: String, required: true},
+    Kaupunki: { type: String, required: true},
+    Operaattori: { type: String, required: true },
+    Kapasiteetti: { type: Number, required: true },
+    x: { type:Number, required: true },
+    y: { type:Number, required: true },
     Starting_station_count: Number,
-    Ending_station_count: Number
+    Ending_station_count: Number,
+    Average_journey_distance_starting: Number,
+    Average_journey_distance_ending: Number,
 });
-  
+
 // Create model objects
 const Journey = mongoose.model('journey', journeysSchema);
 const Station = mongoose.model('station', stationsSchema);
-  
+
 // Export model objects
 module.exports = {
     Journey, Station
