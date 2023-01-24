295100c0844296a4b349b869c1e8e086c255e15c
Toni Virtanen <tonikoodaa@gmail.com>
2023-01-24 13:54:57 +0200

    Separate database calls to different file

--- routes/journeys.js
+++ routes/journeys.js
@@ -2,37 +2,48 @@
 const express = require('express')
 const router = express.Router()
 const { Journey } = require('../models.js');
-const helper = require('../dataHelper');
+const helper = require('../dataParser');
+
+const { fetchJourneysByQuery, fetchTopUsedJourneys, fetchStationUsageInformation } = require('../services/journeys.js');
+
 
 // Define the about route
 router.get('/about', (req, res) => {
-  res.send('Handles access to citybike journeys data')
+    res.status(200).json({ message: "Handles access to citybike journeys data" });
 })
+
+// Get top five most used journeys
+router.get('/topfive', async (req, res) => {
+    try {
+        const topUsed = await fetchTopUsedJourneys();
+        res.status(200).json(topUsed);
+    } catch (err) {
+        res.status(err.status).json(err.message);
+    }
+})
+
 
 // Get journeys sorted skipped and limited, according to request params.
 router.get('/sorted/:page/:limit/:sortBy/:sortOrder/:filter?', async (req, res) => {
     // Construct items for database query. Pass in defaults, if params are missing.
+    let sortingOrder = {};
+    sortingOrder[req.params.sortBy] = (req.params.sortOrder === "asc" ? 1 : -1);
+
     const options = {
         page: parseInt(req.params.page) || 0,
         limit: parseInt(req.params.limit) || 10,
-        sortBy: req.params.sortBy || Departure_station_name,
+        sortingOrder: sortingOrder,
     }
-    let sortingOrder = {};
-    sortingOrder[req.params.sortBy] = (req.params.sortOrder === "-1" ? -1 : 1);
-
-    const query = Journey.find({});
-    query.sort(sortingOrder);
-    query.skip(options.page * options.limit);
-    query.limit(options.limit)
-    query.select('Departure_station_name Return_station_name Covered_distance Duration')
-
-    await query.exec((err, journeys) => {
-            if (err) {
-                res.status(500).json(err);
-                return;
-            }
-            res.status(200).json(journeys);
-        })
+    try {
+        const result = await fetchJourneysByQuery(options);
+        if (result.length === 0) {
+            res.status(400).json({ error: "No journeys found" });
+            return;
+        }
+        res.status(200).json(result);
+    } catch (err) {
+        res.status(err.status).json(err.message);
+    }
 })
 
 // Get information about station usage amount. Params are station names.
@@ -40,10 +51,10 @@ router.get('/usage/:start/:end', async (req, res) => {
     const startName = req.params.start;
     const endName = req.params.end;
     try {
-        const data = await helper.getStationUsageInformation(startName, endName);
+        const data = fetchStationUsageInformation(startName, endName);
         res.status(200).json(data);
     } catch (err) {
-        res.status(500).json(err);
+        res.status(err.status).json(err.message);
     }
 })
 
