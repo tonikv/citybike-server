0206f69a662a8b7ae72489d7288c9dbbf0c48f14
Toni Virtanen <tonikoodaa@gmail.com>
2023-01-24 13:54:07 +0200

    Separate database calls to different file

--- routes/stations.js
+++ routes/stations.js
@@ -1,34 +1,39 @@
 // Station routes for returning information from database about bike stations.
 const express = require('express')
 const router = express.Router()
-const { Station } = require('../models.js');
+const { fetchStations, fetchStation } = require('../services/stations.js');
 
 // Define the about route
 router.get('/about', (req, res) => {
-  res.send('Handles access to citybike stations data')
+  res.status(200).json({ message: "Handles access to citybike stations data" });
 })
 
 // Get all stations sorted by station name
-router.get('/all', async (req, res) => {
-  const stations = await Station.find({})
-    .sort("Nimi")
-    .exec((err, stations) => {
-      if (err) {
-        res.status(500).json(err);
-        return;
-      }
-      res.status(200).json(stations);
-    })
-});
+router.get('/all', async (req, res, next) => {
+  try {
+    const stations = await fetchStations();
+    if (stations.length === 0) {
+      res.status(404).json("No stations found");
+      return;
+    }
+    res.status(200).json(stations);
+  } catch (err) {
+    res.status(err.status).json(err.message);
+  }
+})
 
 // Get station by FID
 router.get('/byFID/:fid', async (req, res) => {
   const queryID = req.params.fid.toString();
   try {
-    const station = await Station.findOne({ "FID": queryID});
+    const station = await fetchStation(queryID);
+    if ((station === null) || (station === undefined)) {
+      res.status(404).json("Station not found");
+      return;
+    }
     res.status(200).json(station);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(err.status).json(err.message);
   }
 })
 
