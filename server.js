adcf05f27301ea9d9d704cdd81dea2f16d417689
Toni Virtanen <tonikoodaa@gmail.com>
2023-01-24 13:48:43 +0200

    Separate listen function

--- server.js
+++ server.js
@@ -1,25 +1,6 @@
 require('dotenv').config();
-const express = require('express');
-const app = express();
-const db = require('./database');
-const cors = require('cors');
-const stationsRouter = require('./routes/stations.js');
-const journeysRouter = require('./routes/journeys.js');
-
+const app = require('./index.js');
 const PORT = process.env.PORT || 3005;
-
-// Middleware
-app.use(cors());
-
-// Routes
-app.get('/', (req, res) => {
-    res.json({
-        message: "Welcome to citybike server",
-        routes: ["/stations/all", "/stations/byFID/param1", "/journeys/sorted/param1/param2/param3/param4"]
-    });
-})
-app.use('/stations', stationsRouter);
-app.use('/journeys', journeysRouter);
 
 app.listen(PORT, () => {
     console.log(`Listening to ${PORT}`);
